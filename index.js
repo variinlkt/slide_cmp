8100a0f591cfcb74ff621c54c34f7828c959f690
linkaitong <linkaitong@beisen.com>
2019-03-02 21:08:20 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+import Slide from './slide'
+new Slide({
+    container: 'slide_container',
+    list: 'slide_list',
+    item: 'slide_item',
+    footer: 'slide_footer_item',
+    auto: false,
+    touch: true,
+    time: 2000,
+    infinite: true
+})
